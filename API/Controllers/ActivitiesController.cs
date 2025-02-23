bbe29aa327a981578fe5b50fad0a919b1ee1af96
buenz <hbueno25@gmail.com>
2025-02-23 00:21:09 -0600

    adding all CRUD

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,11 +1,12 @@
 
+using Application.Activities.Commands;
 using Application.Activities.Queries;
 using Domain;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
-
+// BaseApiController has Mediator so when I inherit, I will have its methods
 public class ActivitiesController() : BaseApiController
 {   // received HTTP Req and sends to application through mediator
     [HttpGet] //api/activities
@@ -17,6 +18,28 @@ public class ActivitiesController() : BaseApiController
     [HttpGet("{id}")]  //api/activities/fdfkd
     public async Task<ActionResult<Activity>> GetActivityDetail(string id)
     {
-        return await Mediator.Send(new GetActivityDetails.Query{Id = id});
+        return await Mediator.Send(new GetActivityDetails.Query { Id = id });
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<string>> CreateActivity(Activity activity)
+    {
+        return await Mediator.Send(new CreateActivity.Command { Activity = activity });
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> EditActivity(Activity activity)
+    {   
+        await Mediator.Send(new EditActivity.Command { Activity = activity });
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteActivity(string id)
+    {
+        await Mediator.Send(new DeleteActivity.Command{ Id = id });
+
+        return Ok();
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,6 @@
 
 using Application.Activities.Queries;
+using Application.Core;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -17,6 +18,7 @@ builder.Services.AddDbContext<Persistence.AppDbContext>(opt => {
 
 builder.Services.AddCors();
 builder.Services.AddMediatR(x => x.RegisterServicesFromAssemblyContaining<GetActivityList.Handler>());
+builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 
 var app = builder.Build();
 
